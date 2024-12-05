1dd000e21ab23948bb86385fd5b781d428a4e2c4
UniEon <admin@vyomtech.com>
2024-12-05 23:27:38 +0800

    Update `date_filter` function to handle integer values
    * Add a check to ensure the value is an integer
    * Convert integer value to a date object with a default day before formatting

--- app/filters.py
+++ app/filters.py
@@ -3,4 +3,6 @@ import datetime
 def date_filter(value, format='%B'):
     if value is None:
         return ''
+    if isinstance(value, int):
+        value = datetime.date(1900, value, 1)
     return value.strftime(format)
